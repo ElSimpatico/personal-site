829bade38e0c706cccc7a7d7e198e78376e245d2
Aaron Velasco Lopez <tiquismiquisss@gmail.com>
2024-02-13 17:55:24 +0100

    feat(ui-kit): add constants and enums in core

---
+++ packages/ui-kit/src/core/constants/common.ts
@@ -0,0 +1,2 @@
+export const FocusableElementSelector =
+    'a[href], button:not([disabled]), input:not([disabled]), select:not([disabled]), textarea:not([disabled]), iframe, [tabindex]';
---
+++ packages/ui-kit/src/core/enums/common.ts
@@ -0,0 +1,4 @@
+export enum KEYBOARD_KEY {
+    TAB = 'Tab',
+    ESCAPE = 'Escape',
+}
