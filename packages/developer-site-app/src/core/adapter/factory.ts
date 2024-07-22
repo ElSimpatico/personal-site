3ca0f19a3fabbcbcb6fb471826e7be5622cdfada
Aaron Velasco Lopez <tiquismiquisss@gmail.com>
2024-07-22 18:21:53 +0200

    refactor(app): adapter factory

--- packages/developer-site-app/src/core/adapter/factory.ts
+++ packages/developer-site-app/src/core/adapter/factory.ts
@@ -1,22 +1,34 @@
 import { FooterAdapter } from './footer';
 import { HeaderAdapter } from './header';
 import { HomeAdapter } from './home';
+import { AboutAdapter } from './about';
+import { Services } from '@core/services';
 
-export type AdapterType = 'footer' | 'header' | 'home';
+export type AdapterType = Exclude<Services, Services.LOCALES>;
 
-export type AdapterInstanceType = FooterAdapter | HeaderAdapter | HomeAdapter;
+export type AdapterInstanceType =
+    | FooterAdapter
+    | HeaderAdapter
+    | HomeAdapter
+    | AboutAdapter;
+
+const AdapterMap: Record<AdapterType, AdapterInstanceType> = {
+    header: new HeaderAdapter(),
+    footer: new FooterAdapter(),
+    home: new HomeAdapter(),
+    about: new AboutAdapter(),
+};
 
 export class AdapterFactory {
-    static create(type: AdapterType): AdapterInstanceType {
-        switch (type) {
-            case 'footer':
-                return new FooterAdapter();
-            case 'header':
-                return new HeaderAdapter();
-            case 'home':
-                return new HomeAdapter();
-            default:
-                return null;
+    static create(type: Services.HEADER): HeaderAdapter;
+    static create(type: Services.FOOTER): FooterAdapter;
+    static create(type: Services.HOME): HomeAdapter;
+    static create(type: Services.ABOUT): AboutAdapter;
+    static create(type: AdapterType) {
+        const adapter = AdapterMap[type];
+        if (!adapter) {
+            throw new Error(`Unknown adapter type ${type}`);
         }
+        return adapter;
     }
 }
